34e22fe5aa6ace1233ea44fc97af09ba159ed7ce
alexjian <jhn840530@gmail.com>
2022-10-08 23:29:23 +0800

    finish

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
 #from .views import article_detail_view,article_list_view
-from .views import ArticleListView,ArticleDetailView,ArticleCreateView,ArticleUpdateView
+from .views import ArticleListView,ArticleDetailView,ArticleCreateView,\
+                    ArticleUpdateView,ArticleDeleteView,CourseView
 
 
 app_name = 'blogs'
@@ -10,6 +11,8 @@ urlpatterns = [
     path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
     path('create/', ArticleCreateView.as_view(), name='article-create'),
     path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
+    path('course/', CourseView.as_view(), name='course-list'),
 
     #path('', article_list_view, name='article-list'),
     #path('<int:my_id>', article_detail_view, name='article-detail'),
--- blogs/views.py
+++ blogs/views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
 from .models import Article
 from .forms import ArticleForm
 from django.views.generic import CreateView,DetailView,ListView,UpdateView,DeleteView
+from django.views import View
+
+class CourseView(View):
+    template_name = 'blogs/course_detail.html'
+    def get(self,request, id = None, *args, **kwargs):
+        context={}
+        if id is not None:
+            id_ = self.kwargs.get('id')
+            obj = get_object_or_404(Article, id = id_)
+            context['object'] = obj
+        return render(request, self.template_name, context)
+
+class ArticleListView(ListView):
+    template_name = 'blogs/list.html'
+    queryset = Article.objects.all()   #default會去找<App>/<model_name>_list.html
 
 class ArticleCreateView(CreateView):
     template_name = 'blogs/create.html'
     form_class = ArticleForm
     queryset = Article.objects.all()
     def form_valid(self, form):
-        print(form.cleaned_data)
+        #print(form.cleaned_data)
         return super().form_valid(form)
-
-class ArticleListView(ListView):
-    template_name = 'blogs/list.html'
-    queryset = Article.objects.all()   #default會去找<App>/<model_name>_list.html
 
 class ArticleDetailView(DetailView):
     template_name = 'blogs/detail.html'
@@ -30,8 +42,17 @@ class ArticleUpdateView(UpdateView):
         id_ = self.kwargs.get('id')
         return get_object_or_404(Article, id = id_)
     def form_valid(self, form):
-        print(form.cleaned_data)
+        #print(form.cleaned_data)
         return super().form_valid(form)
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'blogs/delete.html'
+    def get_object(self):
+        id_ = self.kwargs.get('id')
+        return get_object_or_404(Article, id = id_)
+    def get_success_url(self):
+        return reverse('blogs:article-list')
+
 
 
 
